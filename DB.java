ccf8772737318008a801b234eab9ffe38718901a
HFGq11projekt <q11projekt@gymnaila.eu>
2020-07-05 21:02:01 +0200

    Datenbank angelegt

--- DB.java
+++ DB.java
@@ -1,33 +1,10 @@
 
-/**
- * Beschreiben Sie hier die Klasse DB.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
-public class DB
-{
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
 
-    /**
-     * Konstruktor für Objekte der Klasse DB
-     */
-    public DB()
-    {
-        // Instanzvariable initialisieren
-        x = 0;
+public class DB {
+    
+    public DB() {
+    
+
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
 }
