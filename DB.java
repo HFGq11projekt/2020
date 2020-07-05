97b0b30f11f2c23ac706eda72aaf026c20852595
HFGq11projekt <q11projekt@gymnaila.eu>
2020-07-05 22:49:25 +0200

    Klassen hinzugefügt

--- DB.java
+++ DB.java
@@ -3,52 +3,27 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.util.Random;
 
 public class DB {
-    
-    public DB() {
-        
-        
 
-    }
-    
     public ResultSet getTupleFromDatabase(String table, int ID) {
         ResultSet ergebnis = null;
-        try{
-            ergebnis = connect().executeQuery("SELECT * FROM " + table + " WHERE ID = " + ID);
-        }catch(SQLException e){
-            System.out.println(e.getMessage());
-        }
-        return ergebnis;
-    }
-    
-    public void countTuples(String table) {
-        ResultSet numberOfTuplesSet = null;
-        int numberOfTuples = 0;
-        try{
-            numberOfTuplesSet = connect().executeQuery("SELECT COUNT(ID) FROM " + table);
-            //numberOfTuples = numberOfTuplesSet.getInt("COUNT(ID)");
-        }catch(SQLException e){
-            System.out.println(e.getMessage());
-        }
-        System.out.println(numberOfTuplesSet);
-    }
-    
-    public Statement connect() {
-        
         Connection verbindung = null;
         Statement anfrage = null;
-        
+
         try {
             // Datenbankverbindung herstellen
             verbindung = DriverManager.getConnection("jdbc:sqlite:db/database.db");
             // Objekt erzeugen, das die Abwicklung von Anfragen uebernimmt
             anfrage = verbindung.createStatement();
+            
+            ergebnis = anfrage.executeQuery("SELECT * FROM " + table + " WHERE ID = " + ID);
 
         }catch (SQLException e) {
-            
+
             System.out.println(e.getMessage());
-            
+
         }finally {
             try {
                 if (verbindung != null) {
@@ -59,8 +34,58 @@ public class DB {
             }
         }
         
-        return anfrage;
+        return ergebnis;
+    }
+    
+    public int countTuples(String table) {
+        ResultSet numberOfTuplesSet = null;
+        Connection verbindung = null;
+        Statement anfrage = null;
+        int numberOfTuples = 0;
+
+        try {
+            // Datenbankverbindung herstellen
+            verbindung = DriverManager.getConnection("jdbc:sqlite:db/database.db");
+            // Objekt erzeugen, das die Abwicklung von Anfragen uebernimmt
+            anfrage = verbindung.createStatement();
+            
+            numberOfTuplesSet = anfrage.executeQuery("SELECT COUNT(ID) FROM " + table);
+            numberOfTuples = numberOfTuplesSet.getInt("COUNT(ID)");
+
+        }catch (SQLException e) {
+
+            System.out.println(e.getMessage());
+
+        }finally {
+            try {
+                if (verbindung != null) {
+                    verbindung.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+       
+        return numberOfTuples;
+    }
+
+    public Solution getSolution(){
+     
+        Random rand = new Random();
+        ResultSet solutionSet = getTupleFromDatabase("Diagramme", rand.nextInt(countTuples("Diagramme")+1));
+        Solution solution = null;
         
+        try{
+            String name = solutionSet.getString("Name");
+            String land = solutionSet.getString("Land");
+            int breite = solutionSet.getInt("Breite");
+            int laenge = solutionSet.getInt("Länge");
+            String path = solutionSet.getString("Diagramm");
+            solution = new Solution(name, land, breite, laenge, path);
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return solution;
     }
     
 }
---
+++ Solution.java
@@ -0,0 +1,18 @@
+
+public class Solution
+{
+    String name;
+    String land;
+    int breite;
+    int laenge;
+    String path;
+    
+    public Solution(String name, String land, int breite, int laenge, String path) {
+        this.land = land;
+        this.name = name;
+        this.breite = breite;
+        this.laenge = laenge;
+        this.path = path;
+    }
+
+}
