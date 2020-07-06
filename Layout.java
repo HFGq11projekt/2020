af054002b1d6a9566904fb462c2ae0df196d586e
HFGq11projekt <q11projekt@gymnaila.eu>
2020-07-06 13:10:09 +0200

    Bilder und GUI

---
+++ Layout.java
@@ -0,0 +1,42 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+
+
+public class Layout extends Application
+{
+  @FXML
+  private Button Knopf1;
+  @FXML
+  private Button Knopf2;
+  @FXML
+  private Button Knopf3;
+  @FXML
+  private Button Knopf4;
+  @FXML
+  private ImageView Diagramm;
+   
+    public void start(Stage stage) throws Exception
+    {
+        FXMLLoader loader =  new  FXMLLoader(getClass().getResource("Layout.fxml"));
+       
+        Pane layout = loader.load();
+        Scene scene =  new  Scene(layout);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+  public void setImage()
+  {
+      Image image = new Image("Klimadiagramme/Ottawa.gif");
+      Diagramm.setImage(image);
+  }
+
+
+}
