e759b6c1b934ee8c093df0166041674ee17e0ec1
HFGq11projekt <q11projekt@gymnaila.eu>
2020-07-08 08:42:47 +0200

    Hallo

--- Layout.java
+++ Layout.java
@@ -28,6 +28,7 @@ public class Layout extends Application
     public void start(Stage stage) throws Exception
     {
         FXMLLoader loader =  new  FXMLLoader(getClass().getResource("Layout.fxml"));
+        loader.setController(this);
         
         db = new DB();
         control = new Controller(db);
@@ -40,9 +41,33 @@ public class Layout extends Application
 
   public void setImage()
   {
-      Image image = new Image("Klimadiagramme/Ottawa.gif");
+      Image image = new Image(control.getPath());
       Diagramm.setImage(image);
   }
-
+  public void setAnswers()
+  {
+   var answers = control.getAnswers();  
+   Knopf1.setText(answers[0].name + answers[0].land);
+   Knopf2.setText(answers[1].name + answers[1].land);
+   Knopf3.setText(); 
+   Knopf4.setText(); 
+  }
+  public void Knopf1Gedrueckt()
+  {
+      
+  }
+  public void Knopf2Gedrueckt()
+  {
+      
+  }
+  public void Knopf3Gedrueckt()
+  {
+      
+  }
+  public void Knopf4Gedrueckt()
+  {
+      
+  }
+ 
 
 }
