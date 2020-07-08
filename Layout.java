97d8dfbe4c8f2ece39f06d17452dc0dd4d363ad5
HFGq11projekt <q11projekt@gymnaila.eu>
2020-07-08 09:03:25 +0200

    Juhuu

--- Layout.java
+++ Layout.java
@@ -41,16 +41,16 @@ public class Layout
 
   public void setImage()
   {
-      Image image = new Image(control.getPath());
+      Image image = new Image(control.getPathOfSolution());
       Diagramm.setImage(image);
   }
   public void setAnswers()
   {
    var answers = control.getAnswers();  
-   Knopf1.setText(answers[0].name + answers[0].land);
-   Knopf2.setText(answers[1].name + answers[1].land);
-   Knopf3.setText(); 
-   Knopf4.setText(); 
+   Knopf1.setText(answers[0].name + "," + answers[0].land);
+   Knopf2.setText(answers[1].name + "," + answers[1].land);
+   Knopf3.setText(answers[2].name + "," + answers[2].land); 
+   Knopf4.setText(answers[3].name + "," + answers[3].land); 
   }
   public void Knopf1Gedrueckt()
   {
